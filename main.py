829b5445f6f627c8fceb98833b522bd71b5549ab
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2024-10-21 20:46:28 -0300

    fix: ajustes para adicionar a data presente no nome do arquivo como uma coluna do novo csv

--- main.py
+++ main.py
@@ -6,16 +6,24 @@ import os
 def agrupar_csv_por_nome(diretorio_entrada, diretorio_saida):
     os.makedirs(diretorio_saida, exist_ok=True)
     grupos = defaultdict(list)
-    regex_data = re.compile(r'^\d{8}_')
+    regex_data = re.compile(r'^(\d{8})_')
 
     for arquivo in os.listdir(diretorio_entrada):
         if arquivo.endswith('.csv'):
-            nome_limpo = regex_data.sub('', arquivo)
-            caminho_completo = os.path.join(diretorio_entrada, arquivo)
-            grupos[nome_limpo].append(caminho_completo)
+            match = regex_data.match(arquivo)
+            if match:
+                data = match.group(1)
+                nome_limpo = regex_data.sub('', arquivo)
+                caminho_completo = os.path.join(diretorio_entrada, arquivo)
+                grupos[nome_limpo].append((caminho_completo, data))
 
     for nome, arquivos in grupos.items():
-        dfs = [pd.read_csv(arq, encoding='ISO-8859-1', on_bad_lines='skip', sep=';') for arq in arquivos]
+        dfs = []
+        for arq, data in arquivos:
+            df = pd.read_csv(arq, encoding='ISO-8859-1', on_bad_lines='skip', sep=';', low_memory=False)
+            df.insert(0, 'Data', data)
+            dfs.append(df)
+
         df_combined = pd.concat(dfs, ignore_index=True)
         caminho_saida = os.path.join(diretorio_saida, f'{nome}')
         df_combined.to_csv(caminho_saida, index=False, encoding='ISO-8859-1', sep=';')
